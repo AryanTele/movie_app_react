71e52a7bf46b16f56d53fc6425348a247ab13f80
Aryan Tele <telearyan12@gmail.com>
2024-06-22 12:46:14 +0530

    fetch api respose

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 15573fa
+
+const API_URL = "https://www.omdbapi.com?apikey=15573fa";
+
+const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    searchMovie("Spiderman");
+  });
+  return <h1>App</h1>;
+};
 
 export default App;
